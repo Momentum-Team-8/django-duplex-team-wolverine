576feab4cf3419cdc0c9fa14f9c3dc88ad38fd9f
loganatkinson <logan.atkinson@gmail.com>
2021-06-25 16:46:32 -0400

    defined basic home view

--- snippet/views.py
+++ snippet/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Snippet, User
 
 # Create your views here.
+def home(request):
+    return render(request, "snippet/home.html")
