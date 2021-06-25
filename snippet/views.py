1c1fb1c4e759decd94c531f8e9902619d19499e1
loganatkinson <logan.atkinson@gmail.com>
2021-06-25 18:32:48 -0400

    reset

--- snippet/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render, redirect, get_object_or_404
-from .models import Snippet, User
-
-# Create your views here.
-def home(request):
-    return render(request, "snippet/home.html")
