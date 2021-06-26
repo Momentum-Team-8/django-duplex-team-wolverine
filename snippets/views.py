2ad853694beb4fa5d4a96c78fab66c08a822ccf9
loganatkinson <logan.atkinson@gmail.com>
2021-06-26 18:47:47 -0400

    added login-logout option to homepage and added authentication

--- snippets/views.py
+++ snippets/views.py
@@ -1,10 +1,18 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Snippet, Tags
 from django.utils import timezone
-from django.shortcuts import render, get_object_or_404, redirect
 
 # Create your views here.
 
 
 def homepage(request):
-    return render(request, "snippets/home.html")
+    if request.user.is_authenticated:
+        return redirect("list_snippets")
+    else:
+        return render(request, "snippets/home.html")
+
+@login_required
+def list_snippets(request):
+    snippets = Snippet.objects.all().order_by("created_date")
+    return render(request, "snippets/list_snippets.html", {"snippets": snippets})
