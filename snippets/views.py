1218f6939b83ef680ad09a05620dabc88d446d75
roanblanchard <roan.g.blanchard@icloud.com>
2021-06-27 13:46:59 -0400

    added a form for the user to add their own snippets

--- snippets/views.py
+++ snippets/views.py
@@ -2,6 +2,7 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Snippet, Tags
 from django.utils import timezone
+from .forms import SnippetForm
 
 # Create your views here.
 
@@ -20,3 +21,15 @@ def list_snippets(request):
 def snippet_details(request, pk):
     snippet = get_object_or_404(Snippet, pk=pk)
     return render(request, "snippets/snippet_details.html", {"snippet": snippet})
+
+def add_snippet(request):
+    if request.method == "POST":
+        form = SnippetForm(data=request.POST)
+        if form.is_valid():
+            snippet = form.save(commit=False)
+            snippet.save()
+            return redirect("snippet_details", pk=snippet.pk)
+    else:
+        form = SnippetForm()
+
+    return render(request, "snippets/add_snippet.html", {"form": form})
