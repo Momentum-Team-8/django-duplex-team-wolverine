abd8506432b44d8c829d533f47804e02a8abba2f
roanblanchard <roan.g.blanchard@icloud.com>
2021-06-28 13:20:25 -0400

    Added edit button in the snippet details

--- snippets/urls.py
+++ snippets/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('accounts/', include('registration.backends.simple.urls')),
     path("snippets/<int:pk>", views.snippet_details, name="snippet_details"),
     path("snippets/new", views.add_snippet, name="add_snippet"),
+    path("snippets/<int:pk>/edit", views.edit_snippet, name="edit_snippet"),
 ]
--- snippets/views.py
+++ snippets/views.py
@@ -33,3 +33,16 @@ def add_snippet(request):
         form = SnippetForm()
 
     return render(request, "snippets/add_snippet.html", {"form": form})
+
+
+def edit_snippet(request, pk):
+    snippet = get_object_or_404(Snippet, pk=pk)
+    if request.method == "GET":
+        form = SnippetForm(instance=snippet)
+    else:
+        form = SnippetForm(data=request.POST, instance=snippet)
+        if form.is_valid():
+            form.save()
+            return redirect("list_snippets")
+
+    return render(request, "snippets/edit_snippet.html", {"form": form, "snippet": snippet})
